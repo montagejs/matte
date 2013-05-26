4d41d6a3533013d8989d5c94b7376f51ecfd0357
Pierre Frisch <pierre.frisch@spearway.com>
2013-05-25 20:01:30 -0700

    Fix image component

--- ui/image.reel/image.js
+++ ui/image.reel/image.js
@@ -1,25 +1,30 @@
-
 /**
-    @module "matte/ui/image.reel"
-*/
+ @module "matte/ui/image.reel"
+ */
 
 /*global require,exports */
 var Montage = require("montage").Montage,
     Component = require("montage/ui/component").Component,
-    NativeImage = require("native/ui/image.reel").Image;
+    AbstractImage = require("montage/ui/base/abstract-image").AbstractImage;
 
 /**
- * Input Text
+ * Image
  * @class module:"matte/ui/image.reel".Image
  * @extends module:"native/ui/image.reel".Image
  */
-exports.Image = Montage.create(NativeImage, /** @lends module:"matte/ui/image.reel".Image */ {
+exports.Image = Montage.create(AbstractImage, /** @lends module:"matte/ui/image.reel".Image */ {
+
+    hasTemplate: {
+        value: false
+    },
 
     didCreate: {
-        value: function() {
+        value: function () {
             // Call super method
-            NativeImage.didCreate.call(this);
-            this.classList.add("matte-image");
+            if (AbstractImage.didCreate) {
+                AbstractImage.didCreate.call(this);
+            }
+            this.classList.add("matte-Image");
         }
     }
 
