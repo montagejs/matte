8e5bad1cbb757425a20091e26b4abba694bfe3ad
Mike Czepiel <mike@czepiel.net>
2013-07-09 14:30:09 -0700

    Move input-text from native-input to abstract
    Mainly to pickup the action event that the abstractFextField dispatches

--- ui/input-text.reel/input-text.js
+++ ui/input-text.reel/input-text.js
@@ -2,14 +2,14 @@
     @module "matte/ui/input-text.reel"
 
 /*global require,exports */
-var NativeInputText = require("native/ui/input-text.reel").InputText;
+var AbstractTextField = require("montage/ui/base/abstract-text-field").AbstractTextField;
 
 /**
  * Input Text
  * @class module:"matte/ui/input-text.reel".InputText
  * @extends module:"native/ui/input-text.reel".InputText
  */
-exports.InputText = NativeInputText.specialize(/** @lends module:"matte/ui/input-text.reel".InputText# */ {
+exports.InputText = AbstractTextField.specialize(/** @lends module:"matte/ui/input-text.reel".InputText# */ {
 
     hasTemplate: {value: true},
 
