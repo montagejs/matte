077720c094c14a7c4cdd85903cc5b4b85e94e9ea
cesine <cesine@yahoo.com>
2013-09-17 16:27:15 -0400

    input number extends abstract number field
    instead of native input, now value is forced to be an int or float (a number)
    AbstractNumberField requires plus and minus buttons, but this commit uses the input type=number's built in plus and minus buttons.

--- ui/input-number.reel/input-number.js
+++ ui/input-number.reel/input-number.js
@@ -3,24 +3,19 @@
     @module "matte/ui/input-number.reel"
 */
 
-var NativeInputNumber = require("native/ui/input-number.reel").InputNumber;
+var AbstractNumberField = require("montage/ui/base/abstract-number-field").AbstractNumberField;
 
 /**
- * Wraps the a &lt;input type="date"> element with binding support for the element's standard attributes.
+ * Wraps a <input type="number"> element with binding support for the element's standard attributes 
+   and conversion support from a string to a valid number (integer or float). 
    @class module:"matte/ui/input-number.reel".InputNumber
-   @extends module:"native/ui/input-number.reel".InputNumber
+   @extends module:"montage/ui/base/abstract-number-field".AbstractNumberField
  */
-exports.InputNumber = NativeInputNumber.specialize(/** @lends module:"matte/ui/input-number.reel".InputNumber */{
-
-    hasTemplate: {
-        value: true
-    },
+exports.InputNumber = AbstractNumberField.specialize(/** @lends module:"matte/ui/input-number.reel".InputNumber */{
 
     constructor: {
         value: function InputNumber() {
             this.super();
-            this.classList.add("matte-InputNumber");
-            this.classList.add("matte-InputText");
         }
     }
 
